a61cd2ec0ed037e0aff748f704aa46c7d3a6106e
Kostia Arlouski <kostia.arlouski@universe.com>
2021-02-04 09:44:55 -0500

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  printWidth: 120,
+  semi: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
